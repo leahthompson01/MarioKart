31c5da20b54e5d0bae4b7640e79a4a7130afb871
leahthompson01 <leahmthompson03@gmail.com>
2022-05-15 03:09:44 -0400

    initial commit, all pics and files

---
+++ main.js
@@ -0,0 +1,56 @@
+document.getElementById('yoshi').onclick = yoshiParty
+document.getElementById('peach').onclick = peachParty
+document.getElementById('daisy').onclick = daisyParty
+document.getElementById('mario').onclick = marioParty
+document.getElementById('luigi').onclick = luigiParty
+document.getElementById('wario').onclick = warioParty
+document.getElementById('donkeykong').onclick = donkeykongParty
+document.getElementById('waluigi').onclick = waluigiParty
+document.getElementById('toad').onclick = toadParty
+
+function yoshiParty() {
+    document.querySelector('#picture-holder').style.backgroundImage = 'url(https://i.gifer.com/embedded/download/l0a.gif)'
+    document.querySelector('#picture-holder').style.backgroundSize = 'cover'
+    document.querySelector('#picture-holder').style.backgroundRepeat = 'no-repeat'
+}
+
+function peachParty(){
+    document.querySelector('#picture-holder').style.backgroundImage = 'url(https://i.gifer.com/embedded/download/129q.gif)'
+    document.querySelector('#picture-holder').style.backgroundSize = 'cover'
+    document.querySelector('#picture-holder').style.backgroundRepeat = 'no-repeat'
+}
+function daisyParty(){
+    document.querySelector('#picture-holder').style.backgroundImage = 'url(https://c.tenor.com/6AAroNZrCaMAAAAC/mario-kart-princess-daisy.gif)'
+    document.querySelector('#picture-holder').style.backgroundSize = 'cover'
+    document.querySelector('#picture-holder').style.backgroundRepeat = 'no-repeat'
+}
+function marioParty(){
+    document.querySelector('#picture-holder').style.backgroundImage = 'url(https://storage.googleapis.com/proudcity/elgl/uploads/2018/07/giphy.gif)'
+    document.querySelector('#picture-holder').style.backgroundSize = 'cover'
+    document.querySelector('#picture-holder').style.backgroundRepeat = 'no-repeat'
+}
+function luigiParty(){
+    document.querySelector('#picture-holder').style.backgroundImage = 'url(https://c.tenor.com/f07cjTq-uFAAAAAC/mario-kart.gif)'
+    document.querySelector('#picture-holder').style.backgroundSize = 'cover'
+    document.querySelector('#picture-holder').style.backgroundRepeat = 'no-repeat'
+}
+function warioParty(){
+    document.querySelector('#picture-holder').style.backgroundImage = 'url(https://c.tenor.com/yp-W0GK4IzwAAAAC/mario-kart-mario.gif)'
+    document.querySelector('#picture-holder').style.backgroundSize = 'cover'
+    document.querySelector('#picture-holder').style.backgroundRepeat = 'no-repeat'
+}
+function donkeykongParty(){
+    document.querySelector('#picture-holder').style.backgroundImage = 'url(http://pa1.narvii.com/6499/b17fc41fcc63ebfdfae55e85c2e77f84df6115c0_00.gif)'
+    document.querySelector('#picture-holder').style.backgroundSize = 'cover'
+    document.querySelector('#picture-holder').style.backgroundRepeat = 'no-repeat'
+}
+function waluigiParty(){
+    document.querySelector('#picture-holder').style.backgroundImage = 'url(https://thumbs.gfycat.com/OldWetGull-size_restricted.gif)'
+    document.querySelector('#picture-holder').style.backgroundSize = 'cover'
+    document.querySelector('#picture-holder').style.backgroundRepeat = 'no-repeat'
+}
+function toadParty(){
+    document.querySelector('#picture-holder').style.backgroundImage = 'url(https://assets.teenvogue.com/photos/5994944e84cbab724ff07393/16:9/w_2560%2Cc_limit/2496158-9068096308-elisp.gif)'
+    document.querySelector('#picture-holder').style.backgroundSize = 'cover'
+    document.querySelector('#picture-holder').style.backgroundRepeat = 'no-repeat'
+}
